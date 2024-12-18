6c2f9cad91ce188b50ab0d83501bb755413deafb
niuma_semu <1479564558@qq.com>
2024-12-18 19:38:15 +0800

    unimport

--- sli-dao/src/main/java/com/semu/sli/dao/repository/SliHotSearchRepository.java
+++ sli-dao/src/main/java/com/semu/sli/dao/repository/SliHotSearchRepository.java
@@ -3,8 +3,6 @@ package com.semu.sli.dao.repository;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.semu.sli.dao.entity.SliHotSearchDO;
 
-import java.util.List;
-
 public interface SliHotSearchRepository extends IService<SliHotSearchDO> {
 
 }

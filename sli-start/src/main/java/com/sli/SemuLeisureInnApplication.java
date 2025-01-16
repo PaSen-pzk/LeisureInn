b76a2b28aae8df294da69a6be560c85f1bf968ff
niuma_semu <1479564558@qq.com>
2025-01-16 10:13:05 +0800

    添加单机定时加载本地缓存数据

--- sli-start/src/main/java/com/sli/SemuLeisureInnApplication.java
+++ sli-start/src/main/java/com/sli/SemuLeisureInnApplication.java
@@ -3,12 +3,14 @@ package com.sli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author: Semu
  * @date: 2023/1/16
  * @description: 核心启动类
  */
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.sli"},exclude= {DataSourceAutoConfiguration.class})
 public class SemuLeisureInnApplication {
     public static void main(String[] args) {
